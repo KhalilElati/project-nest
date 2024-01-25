9344232ae8c05732a24599fedf99317f55c092a1
Bzzzd <mohamedaziz.bouzid@carnet.tn>
2024-01-25 10:07:27 +0100

    Added fix to workout module

--- src/workout/workout.module.ts
+++ src/workout/workout.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { WorkoutController } from './workout/workout.controller';
 import { WorkoutController } from './workout.controller';
 import { WorkoutService } from './workout.service';
 
