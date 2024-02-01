a57dec3031278e9c2117ee4c1b224ceeebc35824
Khalil-elati <khalil.elati@carnet.tn>
2024-02-02 00:47:12 +0100

    Adding SWAGGER documentation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // validation
   app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('MythoMorphosis API ')
+    .setDescription('The MythoMorphosis API description')
+    .setVersion('1.0')
+    .addTag('MythoMorphosis')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
